690e88f670dabbbe34876ec69f7e7fc847ec4124
James Ko <jamesqko@gmail.com>
2021-07-01 13:29:55 -0400

    wip

--- plot_results.py
+++ plot_results.py
@@ -1,6 +1,7 @@
 import argparse
 from main import SCRIPT_DIR
 import os
+import shutil
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,15 +26,19 @@ def main():
     plt.ylabel("MAE")
     plt.plot(range(n_epochs), train_losses, label="Training loss")
     plt.plot(range(n_epochs), val_losses, label="Validation loss")
+    plt.legend()
     plt.savefig(f"{figures_dir}/train_and_val_losses_over_time.png")
     plt.clf()
 
     n_bins = len(val_losses_per_bin)
+    bins = [f"{5*i}-{5*i+4}" for i in range(n_bins)]
     plt.xlabel("Age bin")
     plt.ylabel("MAE")
-    plt.plot(range(n_bins), val_losses_per_bin)
+    plt.plot(bins, val_losses_per_bin)
     plt.savefig(f"{figures_dir}/val_losses_per_bin.png")
     plt.clf()
+
+    shutil.copyfile(f"{results_dir}/config.json", f"{figures_dir}/config.json")
 
 if __name__ == '__main__':
     main()
