2f6329e04ef6778597a7e0755ecb57f0eb32dd91
James Ko <jamesqko@gmail.com>
2021-08-06 21:12:19 -0400

    Add the remaining results

--- age_prediction/plot_results.py
+++ age_prediction/plot_results.py
@@ -27,14 +27,29 @@ def plot_losses_over_time(train_losses, val_losses, description, job_id):
     ax.legend()
     fig.savefig(f"{FIGURES_DIR}/{job_id}_train_and_val_losses_over_time.png")
 
-def plot_val_losses_per_bin(ax, preds, description):
+def plot_val_losses_per_5y_bin(ax, preds, description):
     def mae(bin):
-        subjects_in_bin = preds[preds['agebin'] == bin]
+        subjects_in_bin = preds[(preds['age'] // 5) == bin]
+        assert len(subjects_in_bin) > 0
         return np.mean(np.abs(subjects_in_bin['age'] - subjects_in_bin['age_pred']))
+    
+    if description not in ('none / inv', 'none / inv + lds', 'under / none', 'none / none'):
+        return
 
-    bins = range(100)
-    losses_per_bin = [mae(bin) for bin in bins]
-    ax.plot(bins, losses_per_bin, label=description)
+    bins = np.arange(18)
+    losses_per_bin = np.array([mae(bin) for bin in bins])
+    
+    # Smooth losses using a Gaussian kernel
+    smoothed_losses = np.zeros(losses_per_bin.shape)
+    sigma = 1.
+    for bin in bins:
+        kernel = np.exp(-(bins - bin) ** 2 / (2 * sigma ** 2))
+        kernel = kernel / sum(kernel)
+        smoothed_losses[bin] = sum(losses_per_bin * kernel)
+
+    display_bins = [5*bin for bin in bins]
+    ax.set_xticks(display_bins)
+    ax.plot(display_bins, smoothed_losses, label=description)
 
 def main():
     os.makedirs(FIGURES_DIR, exist_ok=True)
@@ -63,9 +78,10 @@ def main():
     val_losses_axs = {}
     for arch in archs:
         fig, ax = plt.subplots()
-        ax.set_title(f"Validation losses per age bin ({arch})")
+        ax.set_title(f"Validation losses per 5-year age bin ({arch})")
         ax.set_xlabel("Age bin")
         ax.set_ylabel("MAE")
+        ax.set_ylim(0, 15)
         val_losses_figs[arch] = fig
         val_losses_axs[arch] = ax
 
@@ -79,7 +95,7 @@ def main():
             description += " + lds"
         
         arch = cfg['arch']
-        plot_val_losses_per_bin(val_losses_axs[arch], val_preds, description)
+        plot_val_losses_per_5y_bin(val_losses_axs[arch], val_preds, description)
 
     for arch in archs:
         val_losses_axs[arch].legend()
