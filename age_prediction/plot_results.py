de960b8df69e975fe5a947f28a1221b91a0560ff
James Ko <jamesqko@gmail.com>
2021-08-12 18:54:38 -0400

    rework plotting code

--- age_prediction/plot_results.py
+++ age_prediction/plot_results.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,91 +16,112 @@ def has_results(job_id):
            os.path.isfile(f"{RESULTS_DIR}/{job_id}/best_model_val_preds.csv") and \
            os.path.isfile(f"{RESULTS_DIR}/{job_id}/config.json")
 
-def plot_losses_over_time(train_losses, val_losses, description, job_id):
-    n_epochs = len(train_losses)
+def describe_job(**cfg):
+    desc = f"{cfg['arch']} / {cfg['sample']} / {cfg['reweight']}"
+    if cfg['lds']:
+        desc += " + lds"
+    return desc
 
-    fig, ax = plt.subplots()
-    ax.set_title(description)
-    ax.set_xlabel("Epoch")
-    ax.set_ylabel("MAE")
-    ax.plot(range(n_epochs), train_losses, label="Training loss")
-    ax.plot(range(n_epochs), val_losses, label="Validation loss")
-    ax.legend()
-    fig.savefig(f"{FIGURES_DIR}/{job_id}_train_and_val_losses_over_time.png")
-
-def plot_val_losses_per_5y_bin(ax, preds, description):
-    def mae(bin):
-        subjects_in_bin = preds[(preds['age'] // 5) == bin]
-        assert len(subjects_in_bin) > 0
-        return np.mean(np.abs(subjects_in_bin['age'] - subjects_in_bin['age_pred']))
-    
-    if description not in ('none / inv', 'none / inv + lds', 'under / none', 'none / none'):
-        return
-
-    bins = np.arange(18)
-    losses_per_bin = np.array([mae(bin) for bin in bins])
-    
-    # Smooth losses using a Gaussian kernel
-    smoothed_losses = np.zeros(losses_per_bin.shape)
-    sigma = 1.
-    for bin in bins:
-        kernel = np.exp(-(bins - bin) ** 2 / (2 * sigma ** 2))
-        kernel = kernel / sum(kernel)
-        smoothed_losses[bin] = sum(losses_per_bin * kernel)
-
-    display_bins = [5*bin for bin in bins]
-    ax.set_xticks(display_bins)
-    ax.plot(display_bins, smoothed_losses, label=description)
-
-def main():
-    os.makedirs(FIGURES_DIR, exist_ok=True)
-
+def load_results():
     job_ids = os.listdir(RESULTS_DIR)
     job_ids = list(filter(has_results, job_ids))
 
-    ## Plot training/validation losses over time
-
+    all_results = {}
     for job_id in job_ids:
         train_losses = np.loadtxt(f"{RESULTS_DIR}/{job_id}/train_losses_over_time.txt")
         val_losses = np.loadtxt(f"{RESULTS_DIR}/{job_id}/val_losses_over_time.txt")
-        with open(f"{RESULTS_DIR}/{job_id}/config.json") as f:
-            cfg = json.load(f)
-
-        description = f"{cfg['arch']} / {cfg['sample']} / {cfg['reweight']}"
-        if cfg['lds']:
-            description += " + lds"
-        
-        plot_losses_over_time(train_losses, val_losses, description, job_id)
-    
-    ## Plot validation losses per age bin
-
-    archs = ['resnet18', 'vgg8']
-    val_losses_figs = {}
-    val_losses_axs = {}
-    for arch in archs:
-        fig, ax = plt.subplots()
-        ax.set_title(f"Validation losses per 5-year age bin ({arch})")
-        ax.set_xlabel("Age bin")
-        ax.set_ylabel("MAE")
-        ax.set_ylim(0, 15)
-        val_losses_figs[arch] = fig
-        val_losses_axs[arch] = ax
-
-    for job_id in job_ids:
         val_preds = pd.read_csv(f"{RESULTS_DIR}/{job_id}/best_model_val_preds.csv")
         with open(f"{RESULTS_DIR}/{job_id}/config.json") as f:
             cfg = json.load(f)
-
-        description = f"{cfg['sample']} / {cfg['reweight']}"
-        if cfg['lds']:
-            description += " + lds"
         
-        arch = cfg['arch']
-        plot_val_losses_per_5y_bin(val_losses_axs[arch], val_preds, description)
+        job_desc = describe_job(**cfg)
+        results = {}
+        results['train_losses'] = train_losses
+        results['val_losses'] = val_losses
+        results['val_preds'] = val_preds
+        results['config'] = cfg
+        all_results[job_desc] = results
+    
+    return all_results
 
-    for arch in archs:
-        val_losses_axs[arch].legend()
-        val_losses_figs[arch].savefig(f"{FIGURES_DIR}/{arch}_val_losses_per_bin.png")
+def plot_losses_over_time(results, fname):
+    train_losses = results['train_losses']
+    val_losses = results['val_losses']
+    cfg = results['config']
+    n_epochs = len(train_losses)
+    
+    title = describe_job(**cfg)
+
+    fig, ax = plt.subplots()
+    ax.set_title(title)
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("MAE")
+    ax.set_ylim(0, 25)
+    ax.plot(range(n_epochs), train_losses, label="Training loss")
+    ax.plot(range(n_epochs), val_losses, label="Validation loss")
+    ax.legend()
+    fig.savefig(f"{FIGURES_DIR}/{fname}")
+    plt.close(fig)
+
+def plot_val_losses(all_results, arch, job_descs, fname, include_baseline=True):
+    def mae(bin, df):
+        subjects_in_bin = df[(df['age'] // 5) == bin]
+        assert len(subjects_in_bin) > 0
+        return np.mean(np.abs(subjects_in_bin['age'] - subjects_in_bin['age_pred']))
+    
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+    ax.set_title(f"Validation losses per 5-year age bin ({arch})")
+    ax.set_xlabel("Age bin")
+    ax.set_ylabel("MAE")
+    ax.set_ylim(0, 15)
+    #ax2.set_ylabel("Number of samples", rotation=270)
+    ax2.set_ylabel("Number of samples")
+    ax2.set_ylim(0, 250)
+    
+    bins = np.arange(18)
+    display_bins = [5*bin for bin in bins]
+
+    if include_baseline:
+        job_descs += ['none / none']
+    for desc in job_descs:
+        key = f"{arch} / {desc}"
+        df = all_results[key]['val_preds']
+        losses_per_bin = np.array([mae(bin, df) for bin in bins])
+        
+        # Smooth losses using a Gaussian kernel
+        smoothed_losses = np.zeros(losses_per_bin.shape)
+        sigma = 1.
+        for bin in bins:
+            kernel = np.exp(-(bins - bin) ** 2 / (2 * sigma ** 2))
+            kernel = kernel / sum(kernel)
+            smoothed_losses[bin] = sum(losses_per_bin * kernel)
+
+        ax.set_xticks(display_bins)
+        is_baseline = (desc == 'none / none')
+        ax.plot(display_bins, smoothed_losses, label=desc, color=('lightgray' if is_baseline else None))
+    
+    ax.legend()
+    bin_counts = [sum((df['age'] // 5) == bin) for bin in bins]
+    ax2.hist(display_bins, bins=display_bins, weights=bin_counts, color='#0f0f0f80')
+    fig.savefig(f"{FIGURES_DIR}/{fname}")
+    plt.close(fig)
+
+def main():
+    shutil.rmtree(FIGURES_DIR)
+    os.makedirs(FIGURES_DIR, exist_ok=True)
+
+    all_results = load_results()
+
+    for results in all_results.values():
+        job_id = results['config']['job_id']
+        plot_losses_over_time(results, f"losses_over_time_{job_id}.png")
+
+    for arch in ('resnet18', 'vgg8'):
+        plot_val_losses(all_results, arch, ['under / none', 'scale-down / none'], f"val_losses_{arch}_under.png")
+        plot_val_losses(all_results, arch, ['over / none', 'scale-up / none'], f"val_losses_{arch}_over.png")
+        plot_val_losses(all_results, arch, ['none / inv', 'none / inv + lds'], f"val_losses_{arch}_inv.png")
+        plot_val_losses(all_results, arch, ['none / sqrt_inv', 'none / sqrt_inv + lds'], f"val_losses_{arch}_sqrt_inv.png")
 
     print("Done")
 
